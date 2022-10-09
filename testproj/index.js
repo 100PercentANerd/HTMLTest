e7afc79e99de778cb77987b203532075988c3c0d
GoldenGamer-2022 <orinhwr@gmail.com>
2022-10-09 18:01:47 +0100

    typescript

---
+++ testproj/index.js
@@ -0,0 +1,16 @@
+// The basic check
+if (document.readyState === 'complete') {
+    // good to go!
+}
+// Polling for the sake of my intern tests
+var interval = setInterval(function () {
+    if (document.readyState === 'complete') {
+        alert("h")
+        clearInterval(interval);
+        done();
+    }
+}, 100);
+//document.getElementById("submit").addEventListener("click", this.submit_alrt, true);
+//function submit_alrt(event){
+//    alert("You pressed the button!");
+//}
