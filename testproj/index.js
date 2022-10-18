79049dd8c7b9b33893b1ad153721e8b2c080d0fd
GoldenGamer-2022 <orinhwr@gmail.com>
2022-10-18 07:24:24 +0100

    stuff

--- testproj/index.js
+++ testproj/index.js
@@ -1,16 +1,5 @@
-// The basic check
-if (document.readyState === 'complete') {
-    // good to go!
-}
-// Polling for the sake of my intern tests
-var interval = setInterval(function () {
-    if (document.readyState === 'complete') {
-        alert("h")
-        clearInterval(interval);
-        done();
-    }
-}, 100);
-//document.getElementById("submit").addEventListener("click", this.submit_alrt, true);
-//function submit_alrt(event){
-//    alert("You pressed the button!");
-//}
+const f = document.getElementById('foo');
+document.addEventListener('click', (ev) => {
+  f.style.transform = `translateY(${ev.clientY - 25}px)`;
+  f.style.transform += `translateX(${ev.clientX - 25}px)`;
+}, false);
